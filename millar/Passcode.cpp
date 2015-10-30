bcc7e7753e508d3bdfeffc680763071b424f32e5
Evan Gillespie <evan.gillespie1@gmail.com>
2015-10-29 20:49:47 -0600

    Created Passcode class

---
+++ millar/Passcode.cpp
@@ -0,0 +1,42 @@
+#include "Arduino.h"
+#include "Passcode.h"
+
+/*
+	constructor for Passcode objects
+*/
+Passcode::Passcode()
+{
+	randomSeed(analogRead(1));	// make sure this is always reading an unconnected pin
+	_passcode = 0;
+	generate();
+}
+
+
+/*
+	generate a 4 digit random number and store it
+*/
+void Passcode::generate()
+{
+	random(0, 10000);
+}
+
+
+/*
+	return the passcode variable
+*/
+int Passcode::get_passcode()
+{
+	return _passcode;
+}
+
+
+/*
+	return a digit from the passcode at a particular index
+	index from 0 to 3
+*/
+int Passcode::get_digit(int index)
+{
+	int num;
+	num = (int) (_passcode / pow(10, 3 - index));
+	return (int)num % 10;
+}
---
+++ millar/millar.ino
@@ -0,0 +1,24 @@
+/****************
+  Arduino Keypad Project for Chris Millar. Title to be Determined
+****************/
+
+#include "Passcode.h"
+
+//////////////////////////
+//	global variables	//
+//////////////////////////
+
+Passcode Passcode();
+
+//////////////////////
+//	intialization	//
+//////////////////////
+
+void setup() {
+
+}
+
+void loop() {
+
+	delay(50);
+}
