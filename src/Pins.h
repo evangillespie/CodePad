7af5391e84fd1026c5be55923f32185a12aab03f
Lowell Smith <lowellbert@gmail.com>
2016-04-07 20:29:53 -0600

    added LED pins

--- src/Pins.h
+++ src/Pins.h
@@ -26,6 +26,21 @@
 
 
 // LED pins
-#define LED_1_PIN 1
-#define LED_2_PIN 3
+#define LED_1_PIN 2  //yellow LEDs
+#define LED_2_PIN 16 //LED Panel#3
+#define LED_3_PIN 23 //Warning Brick LED
+#define LED_4_PIN 25 //Warning Finger LED
+#define LED_5_PIN 27 //Button 3 LED
+#define LED_6_PIN 29 //Button 2 LED
+#define LED_7_PIN 31 //Button 1 LED
+#define LED_8_PIN 39 //CLR LED
+#define LED_9_PIN 41 //OK LED
+#define LED_10_PIN 43 //Keypad LED
+#define LED_11_PIN 45 //Green lights Keypad
+#define LED_12_PIN 46 //power crystal
+#define LED_13_PIN 
+#define LED_14_PIN
+
+
+
 	..
