bb738ae50609bf9f1a724a25c3e5736a63f0cc4f
Lowell Smith <lowellbert@gmail.com>
2016-04-11 21:00:31 -0600

    Added Serial print instructions to pre enable state

--- src/KeypadPreenableState.cpp
+++ src/KeypadPreenableState.cpp
@@ -10,56 +10,71 @@ KeypadPreenableState::KeypadPreenableState() {
 
 long KeypadPreenableState::_get_pause_length(int index) {
 	long _pause_lengths[] = {
-		100,	// pause before the 0th state
-		100,	// pause before the 1st state
-		100,	// 2nd
-		100,	// 3
+		5000,	// pause before the 0th state - Pause 4
+		2000,	// pause before the 1st state 5
+		2000,	// 2nd Pause 6
+		2000,	// 3 Pause 7
+		2000,	// 4 - Pause 8
+		2000,	// 5 - Pause 9
+
 	};
 		
 	return _pause_lengths[index];
 }
 
 int KeypadPreenableState::_get_max_state() {
-	return 3;
+	return 5;
 }
 
 
 void KeypadPreenableState::_dispatcher() {
-
-	// Serial.println("Servo 6 move from 0-500 @ speed=100");
-	// Serial.println("Servo 7 move from 0-500 @ speed=100");
-	// Serial.println("Brick warning LED flashed @ 4 hz");
-	// Serial.println("Brick warning sound triggers when led is high");
-	// Serial.println("Servo 8 move from 0-500 @ speed=100");
-	// Serial.println("Brick warning LED flashed @ 12 hz for 2 seconds");
-	// Serial.println("Brick warning sound triggers when led is high");
-	// Serial.println("Servo 1 move from 500-955 @ speed=60");
-	// Serial.println("Servo 2 move from 500-45 @ speed=60");
-	// Serial.println("Servo 3 move from 1000-460 @ speed=71");
-	// Serial.println("Servo 4 move from 0-540 @ speed=71");
-	// Serial.println("Keypad door sound triggers");
-	// Serial.println("Keypad greedn leds fade from 0-<keypad green pot value> over 3 secs");
-	// Serial.println("Keypad buttons fade in from 0 over 3 seconds");
-	// Serial.println("Timer led is high");
-	// Serial.println("Keypad yellow led fade from max ot 0 over 4 seconds");
-	// Serial.println("Servo 5 move from 0-500 @ speed=100");
-	// Serial.println("Brick LED goes to high");
 	switch(_state_num){
 		case 0:
-			Serial.println("Preenable: Zero");
+			Serial.println("Preenable zero");
+			Serial.println("Servo 6 move from 0-500 @ speed=100");
+			_increment_state();
 			break;
 		case 1:
 			Serial.println("Preenable: One");
+			Serial.println("Servo 7 move from 0-500 @ speed=100");
+			Serial.println("Wait for Servo 7 to reach 500");
+			_increment_state();
 			break;
 		case 2:
 			Serial.println("Preenable: Big Two");
+			Serial.println("Servo 8 move from 0-500 @ speed=100");
+			Serial.println("Brick warning LED flashed @ 4 hz");
+			Serial.println("Brick warning sound triggers when led is high");
+			_increment_state();
 			break;
 		case 3:
 			Serial.println("Preenable: Th-Three");
+			Serial.println("Brick warning LED flashed @ 12 hz for 2 seconds");
+			Serial.println("Servo 1 Move from 500 - 955 @ speed=60");
+			Serial.println("Servo 2 Move from 500 - 45 @ speed of 60");
+			Serial.println("Servo 3 move from 1000 - 460 @ speed of 71");
+			Serial.println("Servo 4 move from 0 - 540 @speed of 71");
+			Serial.println("Keypad door sound triggers");
+			Serial.println("Wait for Servo 1-4 to get to final position");
+			_increment_state();
+			break;
+		case 4:
+			Serial.println("Preenable: Four");
+			Serial.println("keypadgreen LEDs fade from 0 - <keypadgreenpot> value over 3 seconds");
+			Serial.println("keypadbuttons fade from 0 - max over 3 seconds");
+			Serial.println("TimerLED = HIGH");
+			Serial.println("keypadyellow LEDs fade from max - 0 over 4 seconds");
+			Serial.println("servo5 move from 0 - 500 @ speed = 100");
+			Serial.println("Wait for servo5 to reach 500");
+			Serial.println("bricklightLED = HIGH");
+			_increment_state();
+			break;
+		case 5:
+			_increment_state();
 			break;
 		default:
 			Serial.println("You're in a weird state, brah");
 	}
 
-	_increment_state();
+	
 }
